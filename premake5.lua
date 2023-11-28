fbf94cf97b9c3b6e46276b4c042be4b3889c66c2
Benjamin Koskas <benjaminkoskas9@gmail.com>
2023-11-28 12:23:32 +0100

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -1,11 +1,11 @@
-project "GLFW"
+project "glfw"
 	kind "StaticLib"
 	language "C"
 	staticruntime "off"
 	warnings "off"
 
-	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+	targetdir ("bin/" .. OutputDir .. "/%{prj.name}")
+	objdir ("bin-int/" .. OutputDir .. "/%{prj.name}")
 
 	files
 	{
